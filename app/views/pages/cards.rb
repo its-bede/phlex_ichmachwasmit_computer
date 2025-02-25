298ab0a85d7f364e6dd6c61e57ed64d77be970d7
Benjamin Deutscher <ben@byder.io>
2025-02-25 16:29:26 +0100

    feat: add syntax highlighting and snippets for example usages

--- app/views/pages/cards.rb
+++ app/views/pages/cards.rb
@@ -1,30 +1,91 @@
 # frozen_string_literal: true
 
 class Views::Pages::Cards < Views::Base
+  include Phlex::Rails::Helpers::StylesheetLinkTag
+  include Phlex::Rails::Helpers::JavascriptIncludeTag
+
   def view_template
     Layout(title: t("titles.pages.home")) do
+      javascript_include_tag "prism", data: { turbo_track: "reload" }
+      stylesheet_link_tag "prism", "data-turbo-track": "reload"
       main(id: "home", class: "container mt-4 d-flex flex-column justify-content-center gap-5") do
         h1(class: "fs-4 mb-0") { t("headings.pages.home.cards") }
         h2(class: "fs-5 mb-2") { t("sub_headings.pages.cards") }
-        div(class: "d-flex gap-3") do
-          render Card.new do |card|
-            card.body { t('descriptions.pages.cards.with-body-only') }
+        div(class: "row row-gap-5") do
+          div(class: 'col-6') do
+            render Card.new do |card|
+              card.body { t('descriptions.pages.cards.with-body-only') }
+            end
+          end
+          div(class: 'col-6') do
+            pre do
+              code(class: "language-ruby") do
+                <<~RUBY
+                  render Card.new do |card|
+                    card.body { t('descriptions.pages.cards.with-body-only') }
+                  end
+                RUBY
+              end
+            end
           end
 
-          render Card.new do |card|
-            card.header { t("descriptions.pages.cards.card-header") }
-            card.body { t('descriptions.pages.cards.with-header-body') }
+          div(class: 'col-6') do
+            render Card.new do |card|
+              card.header { t("descriptions.pages.cards.card-header") }
+              card.body { t('descriptions.pages.cards.with-header-body') }
+            end
+          end
+          div(class: 'col-6') do
+            pre do
+              code(class: "language-ruby") do
+                <<~RUBY
+                  render Card.new do |card|
+                    card.header { t("descriptions.pages.cards.card-header") }
+                    card.body { t('descriptions.pages.cards.with-header-body') }
+                  end
+                RUBY
+              end
+            end
           end
 
-          render Card.new(title: t("descriptions.pages.cards.title")) do |card|
-            card.header { t("descriptions.pages.cards.card-header") }
-            card.body { t("descriptions.pages.cards.with-header-body-and-title") }
+          div(class: 'col-6') do
+            render Card.new(title: t("descriptions.pages.cards.title")) do |card|
+              card.header { t("descriptions.pages.cards.card-header") }
+              card.body { t("descriptions.pages.cards.with-header-body-and-title") }
+            end
+          end
+          div(class: 'col-6') do
+            pre do
+              code(class: "language-ruby") do
+                <<~RUBY
+                  render Card.new(title: t("descriptions.pages.cards.title")) do |card|
+                    card.header { t("descriptions.pages.cards.card-header") }
+                    card.body { t("descriptions.pages.cards.with-header-body-and-title") }
+                  end
+                RUBY
+              end
+            end
           end
 
-          render Card.new do |card|
-            card.header { t("descriptions.pages.cards.card-header") }
-            card.body { t("descriptions.pages.cards.with-header-body-and-footer") }
-            card.footer { t("descriptions.pages.cards.card-footer") }
+          div(class: 'col-6') do
+            render Card.new do |card|
+              card.header { t("descriptions.pages.cards.card-header") }
+              card.body { t("descriptions.pages.cards.with-header-body-and-footer") }
+              card.footer { t("descriptions.pages.cards.card-footer") }
+            end
+          end
+          div(class: 'col-6') do
+            pre do
+              code(class: "language-ruby") do
+                <<~RUBY
+                  render Card.new do |card|
+                    card.header { t("descriptions.pages.cards.card-header") }
+                    card.body { t("descriptions.pages.cards.with-header-body-and-footer") }
+                    card.footer { t("descriptions.pages.cards.card-footer") }
+                  end
+                RUBY
+              end
+            end
           end
         end
 
--- app/views/pages/modals.rb
+++ app/views/pages/modals.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class Views::Pages::Modals < Views::Base
+  include Phlex::Rails::Helpers::StylesheetLinkTag
+  include Phlex::Rails::Helpers::JavascriptIncludeTag
+
   def view_template
     Layout(title: t("titles.pages.home")) do
+      javascript_include_tag "prism", data: { turbo_track: "reload" }
+      stylesheet_link_tag "prism", "data-turbo-track": "reload"
       main(id: "home", class: "container mt-4 d-flex flex-column justify-content-center gap-5") do
         h1(class: "fs-4 mb-3") { t("headings.pages.home") }
         div(class: "d-flex gap-3") do
@@ -14,6 +19,27 @@ class Views::Pages::Modals < Views::Base
 
               modal.body do
                 p { t(".default-modal.body") }
+                pre do
+                  code(class: "language-ruby") do
+                    <<~RUBY
+                      render Modal.new(id: "default-modal") do |modal|
+                        modal.parts do
+                          modal.header do
+                            p { t(".default-modal.header") }
+                          end
+
+                          modal.body do
+                            p { t(".default-modal.body") }
+                          end
+
+                          modal.footer do
+                            p { t(".default-modal.footer") }
+                          end
+                        end
+                      end
+                    RUBY
+                  end
+                end
               end
 
               modal.footer do
@@ -23,7 +49,210 @@ class Views::Pages::Modals < Views::Base
           end
 
           button(class: "btn btn-primary", data: { bs: { toggle: "modal", target: "#default-modal" } }) {
-            "launch modal"
+            "default modal"
+          }
+
+          render Modal.new(id: "sm-modal", size: 'sm') do |modal|
+            modal.parts do
+              modal.header do
+                p { t(".default-modal.header") }
+              end
+
+              modal.body do
+                p { t(".default-modal.body") }
+                pre do
+                  code(class: "language-ruby") do
+                    <<~RUBY
+                      render Modal.new(id: "sm-modal", size: 'sm') do |modal|
+                        modal.parts do
+                          modal.header do
+                            p { t(".default-modal.header") }
+                          end
+
+                          modal.body do
+                            p { t(".default-modal.body") }
+                          end
+
+                          modal.footer do
+                            p { t(".default-modal.footer") }
+                          end
+                        end
+                      end
+                    RUBY
+                  end
+                end
+              end
+
+              modal.footer do
+                p { t(".default-modal.footer") }
+              end
+            end
+          end
+
+          button(class: "btn btn-primary", data: { bs: { toggle: "modal", target: "#sm-modal" } }) {
+            "sm modal"
+          }
+
+          render Modal.new(id: "lg-modal", size: 'lg') do |modal|
+            modal.parts do
+              modal.header do
+                p { t(".default-modal.header") }
+              end
+
+              modal.body do
+                p { t(".default-modal.body") }
+                pre do
+                  code(class: "language-ruby") do
+                    <<~RUBY
+                      render Modal.new(id: "lg-modal", size: "lg") do |modal|
+                        modal.parts do
+                          modal.header do
+                            p { t(".default-modal.header") }
+                          end
+
+                          modal.body do
+                            p { t(".default-modal.body") }
+                          end
+
+                          modal.footer do
+                            p { t(".default-modal.footer") }
+                          end
+                        end
+                      end
+                    RUBY
+                  end
+                end
+              end
+
+              modal.footer do
+                p { t(".default-modal.footer") }
+              end
+            end
+          end
+
+          button(class: "btn btn-primary", data: { bs: { toggle: "modal", target: "#lg-modal" } }) {
+            "lg modal"
+          }
+
+          render Modal.new(id: "xl-modal", size: "xl") do |modal|
+            modal.parts do
+              modal.header do
+                p { t(".default-modal.header") }
+              end
+
+              modal.body do
+                p { t(".default-modal.body") }
+                pre do
+                  code(class: "language-ruby") do
+                    <<~RUBY
+                      render Modal.new(id: "xl-modal", size: "xl") do |modal|
+                        modal.parts do
+                          modal.header do
+                            p { t(".default-modal.header") }
+                          end
+
+                          modal.body do
+                            p { t(".default-modal.body") }
+                          end
+
+                          modal.footer do
+                            p { t(".default-modal.footer") }
+                          end
+                        end
+                      end
+                    RUBY
+                  end
+                end
+              end
+
+              modal.footer do
+                p { t(".default-modal.footer") }
+              end
+            end
+          end
+
+          button(class: "btn btn-primary", data: { bs: { toggle: "modal", target: "#xl-modal" } }) {
+            "xl modal"
+          }
+
+          render Modal.new(id: "dismissible-modal", dismissible: true) do |modal|
+            modal.parts do
+              modal.header do
+                p { t(".dismissible-modal.header") }
+              end
+
+              modal.body do
+                p { t(".dismissible-modal.body_html") }
+                pre do
+                  code(class: "language-ruby") do
+                    <<~RUBY
+                      render Modal.new(id: "dismissible-modal", dismissible: true) do |modal|
+                        modal.parts do
+                          modal.header do
+                            p { t(".dismissible-modal.header") }
+                          end
+
+                          modal.body do
+                            p { t(".dismissible-modal.body_html") }
+                          end
+                        end
+                      end
+                    RUBY
+                  end
+                end
+              end
+
+              modal.footer
+            end
+          end
+
+          button(class: "btn btn-primary", data: { bs: { toggle: "modal", target: "#dismissible-modal" } }) {
+            "dismissible modal"
+          }
+
+          render Modal.new(id: "static-backdrop-modal", static_backdrop: true) do |modal|
+            modal.parts do
+              modal.header do
+                p { t(".static-backdrop-modal.header") }
+              end
+
+              modal.body do
+                p { t(".static-backdrop-modal.body_html") }
+                pre do
+                  code(class: "language-ruby") do
+                    <<~RUBY
+                      render Modal.new(id: "static-backdrop-modal", static_backdrop: true) do |modal|
+                        modal.parts do
+                          modal.header do
+                            p { t(".static-backdrop-modal.header") }
+                          end
+
+                          modal.body do
+                            p { t(".static-backdrop-modal.body_html") }
+                          end
+
+                          modal.footer do
+                            button(type: "button", class: "btn btn-secondary", data_bs_dismiss: "modal") do
+                              t('buttons.modal.close')
+                            end
+                          end
+                        end
+                      end
+                    RUBY
+                  end
+                end
+              end
+
+              modal.footer do
+                button(type: "button", class: "btn btn-secondary", data_bs_dismiss: "modal") do
+                  t('buttons.modal.close')
+                end
+              end
+            end
+          end
+
+          button(class: "btn btn-primary", data: { bs: { toggle: "modal", target: "#static-backdrop-modal" } }) {
+            "static backdrop modal"
           }
         end
 
